c012a327105d9684b22b27422ebc667579939dbd
José <clasesvirtualez96@gmail.com>
2026-01-04 13:40:07 -0500

    feat: info centralizada + footer actualizado

---
+++ src/core/config.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  contact: {
+    email: "contacto@sudolabs.space",
+    phone: "+51 923 384 303",
+    whatsapp: "51923384303", // Formato para API de Whatsapp
+    address: "Huancayo, Perú"
+  },
+  social: {
+    facebook: "https://www.facebook.com/profile.php?id=61585696276461",
+    instagram: "https://www.instagram.com/sudolabsperu/"
+  }
+};
